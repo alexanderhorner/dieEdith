cc9b4f36e6bbd0b2a957d0a898cb60806881a4be
Alexander Horner <alexanderhorner@alex.local>
2019-04-07 15:17:34 +0200

    travis-ci

--- test.rb
+++ test.rb
@@ -3,16 +3,14 @@ require 'selenium-webdriver'
 
 # Input capabilities
 caps = Selenium::WebDriver::Remote::Capabilities.new
-caps['browserstack.local'] = 'true'
-caps['browserstack.localIdentifier'] = ENV['BROWSERSTACK_LOCAL_IDENTIFIER']
 caps.setCapability("browserName", "android");
-caps.setCapability("device", "Samsung Galaxy S7");
-caps.setCapability("realMobile", "true");
-caps.setCapability("os_version", "6.0");
+    caps.setCapability("device", "Samsung Galaxy S7");
+    caps.setCapability("realMobile", "true");
+    caps.setCapability("os_version", "6.0");
 
 driver = Selenium::WebDriver.for(:remote,
-  :url => "http://USERNAME:ACCESS_KEY@hub-cloud.browserstack.com/wd/hub",
-:desired_capabilities => caps)
+  :url => "http://alexanderhorner3:ACCESS_KEY@hub-cloud.browserstack.com/wd/hub",
+  :desired_capabilities => caps)
 driver.navigate.to "http://www.google.com"
 element = driver.find_element(:name, 'q')
 element.send_keys "BrowserStack"
