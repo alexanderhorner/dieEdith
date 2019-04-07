3a4f06dd50c4db3ca4baf7ec6c511d8ff33974bf
Alexander Horner <alexanderhorner@alex.local>
2019-04-07 15:23:25 +0200

    test.rb

--- test.rb
+++ test.rb
@@ -1,19 +1,20 @@
-require 'rubygems'
-require 'selenium-webdriver'
-
-# Input capabilities
-caps = Selenium::WebDriver::Remote::Capabilities.new
-caps.setCapability("browserName", "android");
-    caps.setCapability("device", "Samsung Galaxy S7");
-    caps.setCapability("realMobile", "true");
-    caps.setCapability("os_version", "6.0");
-
-driver = Selenium::WebDriver.for(:remote,
-  :url => "http://alexanderhorner3:ACCESS_KEY@hub-cloud.browserstack.com/wd/hub",
-  :desired_capabilities => caps)
-driver.navigate.to "http://www.google.com"
-element = driver.find_element(:name, 'q')
-element.send_keys "BrowserStack"
-element.submit
-puts driver.title
-driver.quit
+# require 'rubygems'
+# require 'selenium-webdriver'
+#
+# # Input capabilities
+# caps = Selenium::WebDriver::Remote::Capabilities.new
+# caps.setCapability("browserName", "android");
+#     caps.setCapability("device", "Samsung Galaxy S7");
+#     caps.setCapability("realMobile", "true");
+#     caps.setCapability("os_version", "6.0");
+#
+# driver = Selenium::WebDriver.for(:remote,
+#   :url => "http://alexanderhorner3:ACCESS_KEY@hub-cloud.browserstack.com/wd/hub",
+#   :desired_capabilities => caps)
+# driver.navigate.to "http://www.google.com"
+# element = driver.find_element(:name, 'q')
+# element.send_keys "BrowserStack"
+# element.submit
+# puts driver.title
+# driver.quit
+puts test
